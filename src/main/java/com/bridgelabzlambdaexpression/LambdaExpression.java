e90e50d00888f1725394dfb491f9c19937bbd239
prashantn/12 <prashantnangare0872@gmail.com>
2024-10-10 14:20:37 +0530

    Added: Math Operation App to perform Math Functions  Addition, Subtraction and Division

--- src/main/java/com/bridgelabzlambdaexpression/LambdaExpression.java
+++ src/main/java/com/bridgelabzlambdaexpression/LambdaExpression.java
@@ -1,8 +1,30 @@
 package com.bridgelabzlambdaexpression;
 
+@FunctionalInterface
+interface MathOperation {
+    double operate(double a, double b);
+}
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        System.out.println(" Wlecome in Lambda Expression Problem");
+        // Lambda for Addition
+        MathOperation addition = (a, b) -> a + b;
+
+        // Lambda for Subtraction
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // Lambda for Division
+        MathOperation division = (a, b) -> {
+            if (b != 0) {
+                return a / b;
+            } else {
+                throw new ArithmeticException("Division by zero is not allowed.");
+            }
+        };
+
+
+        System.out.println("Addition: " + addition.operate(10, 5));
+        System.out.println("Subtraction: " + subtraction.operate(10, 5));
+        System.out.println("Division: " + division.operate(10, 2));
     }
 }
