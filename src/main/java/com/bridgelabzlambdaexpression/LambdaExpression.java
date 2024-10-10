870d049a00cd107414f067872ebf0cb4e8027ca7
prashantn/12 <prashantnangare0872@gmail.com>
2024-10-10 14:12:36 +0530

    Added: Welcome in Lambda Expression Problem

---
+++ src/main/java/com/bridgelabzlambdaexpression/LambdaExpression.java
@@ -0,0 +1,8 @@
+package com.bridgelabzlambdaexpression;
+
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        System.out.println(" Wlecome in Lambda Expression Problem");
+    }
+}
